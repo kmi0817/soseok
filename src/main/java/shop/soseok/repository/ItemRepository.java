9b4e09ecccaad201fa2bbf48cdf37e87a89fe669
kmi0817 <kmi0817@naver.com>
2024-07-22 23:21:08 +0900

    add: JpaRepository 구현 및 상품명으로 상품 찾는 메서드 추가

--- src/main/java/shop/soseok/repository/ItemRepository.java
+++ src/main/java/shop/soseok/repository/ItemRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import shop.soseok.domain.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByName(String name);
 }
