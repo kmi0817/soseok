b639805254710cf7ad94dfb5f3f5e19f8f3dcda0
kmi0817 <kmi0817@naver.com>
2024-07-24 22:42:32 +0900

    chore: dto 클래스명 수정 및 변수 수정
    상품 등록 시 사용하는 DTO를 상품 수정에서도 재활용하기 위해 클래스명을 변경했다.

--- src/main/java/shop/soseok/controller/ItemController.java
+++ src/main/java/shop/soseok/controller/ItemController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import shop.soseok.domain.Item;
-import shop.soseok.dto.PostItemRequest;
+import shop.soseok.dto.PostPutItemRequest;
 import shop.soseok.service.ItemService;
 
 import java.util.List;
@@ -24,8 +24,8 @@ public class ItemController {
     }
 
     @PostMapping()
-    public Object saveItem(@RequestBody @Valid PostItemRequest postItemRequest, BindingResult bindingResult) {
-        log.info(String.valueOf(postItemRequest));
+    public Object saveItem(@RequestBody @Valid PostPutItemRequest postPutItemRequest, BindingResult bindingResult) {
+        log.info(String.valueOf(postPutItemRequest));
 
         if (bindingResult.hasErrors()) {
             log.info("postItemRequest Errors = {}", bindingResult);
@@ -33,7 +33,7 @@ public class ItemController {
             return bindingResult.getAllErrors();
         }
 
-        return itemService.save(postItemRequest.toEntity());
+        return itemService.save(postPutItemRequest.toEntity());
     }
 
     @GetMapping("/{id}")
